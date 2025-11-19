0a131fb8330b91f0319dd6674c14c8774d0c72ec
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2025-11-19 20:49:52 +0200

    02-database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import {Button} from "@/components/ui/button"
 
-const Page = () => {
+const Page = async () => {
 
     return <div className="min-h-screen w-full flex items-center justify-center">
         <Button>
