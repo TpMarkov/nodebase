d2ac43e13990a035a53a009df13060c214d2f64f
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2025-11-30 15:17:28 +0200

    add openai to the executor-registry

--- src/features/executions/lib/executor-registry.ts
+++ src/features/executions/lib/executor-registry.ts
@@ -1,10 +1,11 @@
-import {NodeType} from "@/generated/prisma/enums";
-import {NodeExecutor} from "@/features/executions/types";
-import {manualTriggerExecutor} from "@/features/triggers/components/manual-trigger/executor";
-import {httpRequestExecutor} from "@/features/executions/components/http-request/executor";
-import {googleFormTriggerExecutor} from "@/features/triggers/components/google-form-trigger/executor";
-import {stripeTriggerExecutor} from "@/features/triggers/components/stripe-trigger/executor";
-import {geminiExecutor} from "@/features/executions/components/gemini/executor";
+import { NodeType } from "@/generated/prisma/enums";
+import { NodeExecutor } from "@/features/executions/types";
+import { manualTriggerExecutor } from "@/features/triggers/components/manual-trigger/executor";
+import { httpRequestExecutor } from "@/features/executions/components/http-request/executor";
+import { googleFormTriggerExecutor } from "@/features/triggers/components/google-form-trigger/executor";
+import { stripeTriggerExecutor } from "@/features/triggers/components/stripe-trigger/executor";
+import { geminiExecutor } from "@/features/executions/components/gemini/executor";
+import { openAiExecutor } from "@/features/executions/components/openAI/executor";
 
 export const executorRegistry: Record<NodeType, NodeExecutor> = {
   [NodeType.MANUAL_TRIGGER]: manualTriggerExecutor,
@@ -13,9 +14,8 @@ export const executorRegistry: Record<NodeType, NodeExecutor> = {
   [NodeType.GOOGLE_FORM_TRIGGER]: googleFormTriggerExecutor,
   [NodeType.STRIPE_TRIGGER]: stripeTriggerExecutor,
   [NodeType.ANTHROPIC]: manualTriggerExecutor,
-  [NodeType.OPENAI]: manualTriggerExecutor,
-  [NodeType.GEMINI]:
-  geminiExecutor
+  [NodeType.OPENAI]: openAiExecutor,
+  [NodeType.GEMINI]: geminiExecutor
 }
 
 export const getExecutor = (type: NodeType): NodeExecutor => {
