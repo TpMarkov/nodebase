0dbd79eb6a254ff7ebd957dbdabb87b207a59889
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2025-12-01 20:04:28 +0200

    feat: Add credential management UI for creating and editing API keys.

--- src/app/(dashboard)/(rest)/credentials/[credentialId]/page.tsx
+++ src/app/(dashboard)/(rest)/credentials/[credentialId]/page.tsx
@@ -1,18 +1,36 @@
-import React from 'react'
-import {requireAuth} from "@/lib/auth-utils";
+import React, { Suspense } from 'react'
+import { requireAuth } from "@/lib/auth-utils";
+import { CredentialView } from "@/features/credentials/components/credential";
+import { prefetchCredential } from '@/features/credentials/server/prefetch';
+import { ErrorBoundary } from 'react-error-boundary';
+import { CredentialError, CredentialLoading } from '@/features/credentials/components/credentials';
+import { HydrateClient } from '@/trpc/server';
 
 interface PageProps {
-    params: Promise<{
-        credentialId: string;
-    }>
+  params: Promise<{
+    credentialId: string;
+  }>
 }
 
-const Page = async ({params}: PageProps) => {
-    const {credentialId} = await params
-    await requireAuth()
+const Page = async ({ params }: PageProps) => {
+  await requireAuth()
 
-    return (
-        <div>Credential ID : {credentialId}</div>
-    )
+  const { credentialId } = await params
+
+  prefetchCredential(credentialId)
+
+  return (
+    <div className={"p-4 md:px-10 md:py-6 h-full"}>
+      <div className={"mx-auto max-w-screen-md w-full flex flex-col gap-y-8 h-full"}>
+        <HydrateClient>
+          <ErrorBoundary fallback={<CredentialError />}>
+            <Suspense fallback={<CredentialLoading />}>
+              <CredentialView credentialId={credentialId} />
+            </Suspense>
+          </ErrorBoundary>
+        </HydrateClient>
+      </div>
+    </div>
+  )
 }
 export default Page
