be4d782fcefead887980a914d2d2e11da98eb699
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2025-12-02 23:24:22 +0200

    27-executions-history

--- src/app/(dashboard)/(rest)/executions/page.tsx
+++ src/app/(dashboard)/(rest)/executions/page.tsx
@@ -1,16 +1,21 @@
-import React, { Suspense } from "react";
-import { requireAuth } from "@/lib/auth-utils";
-import { SearchParams } from "nuqs";
-import { HydrateClient } from "@/trpc/server";
-import { ErrorBoundary } from "react-error-boundary";
-import { executionsParamsLoader } from "@/features/executions/server/params-loader";
-import { prefetchExecutions } from "@/features/executions/server/prefetch";
+import React, {Suspense} from "react";
+import {requireAuth} from "@/lib/auth-utils";
+import {SearchParams} from "nuqs";
+import {HydrateClient} from "@/trpc/server";
+import {ErrorBoundary} from "react-error-boundary";
+import {executionsParamsLoader} from "@/features/executions/server/params-loader";
+import {prefetchExecutions} from "@/features/executions/server/prefetch";
+import ExecutionsList, {
+  ExecutionsContainer,
+  ExecutionsError,
+  ExecutionsLoading
+} from "@/features/executions/executions";
 
 type Props = {
   searchParams: Promise<SearchParams>;
 };
 
-const Page = async ({ searchParams }: Props) => {
+const Page = async ({searchParams}: Props) => {
   await requireAuth();
 
   const params = await executionsParamsLoader(searchParams);
@@ -18,13 +23,15 @@ const Page = async ({ searchParams }: Props) => {
   prefetchExecutions(params);
 
   return (
-      <>
+      <ExecutionsContainer>
         <HydrateClient>
-          <ErrorBoundary fallback={<></>}>
-            <Suspense fallback={<></>}>TODO: List for executions</Suspense>
+          <ErrorBoundary fallback={<ExecutionsError/>}>
+            <Suspense fallback={<ExecutionsLoading/>}>
+              <ExecutionsList/>
+            </Suspense>
           </ErrorBoundary>
         </HydrateClient>
-      </>
+      </ExecutionsContainer>
   );
 };
 export default Page;
--- src/features/executions/executions.tsx
+++ src/features/executions/executions.tsx
@@ -16,16 +16,16 @@ import {
   LoadingView,
 } from "@/components/entity-components";
 
-import { useRouter } from "next/navigation";
-import { useExecutionsParams } from "@/features/executions/hooks/use-executions-params";
-import { formatDistanceToNow } from "date-fns";
-import { ExecutionStatus } from "@/generated/prisma/browser";
+import {useRouter} from "next/navigation";
+import {useExecutionsParams} from "@/features/executions/hooks/use-executions-params";
+import {formatDistanceToNow} from "date-fns";
+import {ExecutionStatus} from "@/generated/prisma/browser";
 
-import type { Credential } from "@/generated/prisma/browser";
-import { CredentialType } from "@/generated/prisma/enums";
+import type {Credential} from "@/generated/prisma/browser";
+import {CredentialType} from "@/generated/prisma/enums";
 
 import Image from "next/image";
-import type { Execution } from "@/generated/prisma/client";
+import type {Execution} from "@/generated/prisma/client";
 import {
   CheckCircleIcon,
   ClockIcon,
@@ -37,39 +37,39 @@ const ExecutionsList = () => {
   const executions = useSuspenseExecutions();
 
   return (
-    <EntityList
-      items={executions.data.items}
-      getKey={(execution) => execution.id}
-      emptyView={<ExecutionsEmpty />}
-      renderItem={(execution) => <ExecutionItem data={execution} />}
-    />
+      <EntityList
+          items={executions.data.items}
+          getKey={(execution) => execution.id}
+          emptyView={<ExecutionsEmpty/>}
+          renderItem={(execution) => <ExecutionItem data={execution}/>}
+      />
   );
 };
 export default ExecutionsList;
 
 export const ExecutionsHeader = () => {
   return (
-    <>
-      <EntityHeader
-        title={"Executions"}
-        description={"View your execution workflows history"}
-      />
-    </>
+      <>
+        <EntityHeader
+            title={"Executions"}
+            description={"View your execution workflows history"}
+        />
+      </>
   );
 };
 
 export const ExecutionsContainer = ({
-  children,
-}: {
+                                      children,
+                                    }: {
   children: React.ReactNode;
 }) => {
   return (
-    <EntityContainer
-      header={<ExecutionsHeader />}
-      pagination={<ExecutionsPagination />}
-    >
-      {children}
-    </EntityContainer>
+      <EntityContainer
+          header={<ExecutionsHeader/>}
+          pagination={<ExecutionsPagination/>}
+      >
+        {children}
+      </EntityContainer>
   );
 };
 
@@ -78,59 +78,54 @@ export const ExecutionsPagination = () => {
   const [params, setParams] = useExecutionsParams();
 
   return (
-    <EntityPagination
-      disabled={executions.isFetching}
-      page={executions.data.page}
-      totalPages={executions.data.totalPages}
-      onPageChange={(page) => {
-        setParams({ ...params, page });
-      }}
-    />
+      <EntityPagination
+          disabled={executions.isFetching}
+          page={executions.data.page}
+          totalPages={executions.data.totalPages}
+          onPageChange={(page) => {
+            setParams({...params, page});
+          }}
+      />
   );
 };
 
 export const ExecutionsLoading = () => {
-  return <LoadingView message={"Loading Executions..."} />;
+  return <LoadingView message={"Loading Executions..."}/>;
 };
 
 export const ExecutionsError = () => {
-  return <ErrorView message={"Error loading Executions"} />;
+  return <ErrorView message={"Error loading Executions"}/>;
 };
 
 export const ExecutionsEmpty = () => {
   const router = useRouter();
 
   return (
-    <EmptyView
-      message={
-        "You haven't created any executions yet. Get started by running your first workflow."
-      }
-    />
+      <EmptyView
+          message={
+            "You haven't created any executions yet. Get started by running your first workflow."
+          }
+      />
   );
 };
 
-const credentialLogos: Record<CredentialType, string> = {
-  [CredentialType.OPENAI]: "/logos/openai.svg",
-  [CredentialType.GEMINI]: "/logos/gemini.svg",
-  [CredentialType.ANTHROPIC]: "/logos/anthropic.svg",
-};
 
 const getStatusIcon = (status: ExecutionStatus) => {
   switch (status) {
     case ExecutionStatus.SUCCESS:
-      return <CheckCircleIcon className="size-5 text-green-600" />;
+      return <CheckCircleIcon className="size-5 text-green-600"/>;
     case ExecutionStatus.FAILED:
-      return <XCircleIcon className="size-5 text-red-600" />;
+      return <XCircleIcon className="size-5 text-red-600"/>;
     case ExecutionStatus.RUNNING:
-      return <Loader2Icon className="size-5 text-blue-600 animate-spin" />;
+      return <Loader2Icon className="size-5 text-blue-600 animate-spin"/>;
     default:
-      return <ClockIcon className="size-5 text-muted-foreground" />;
+      return <ClockIcon className="size-5 text-muted-foreground"/>;
   }
 };
 
 export const ExecutionItem = ({
-  data,
-}: {
+                                data,
+                              }: {
   data: Execution & {
     workflow: {
       id: string;
@@ -139,30 +134,30 @@ export const ExecutionItem = ({
   };
 }) => {
   const duration = data.completedAt
-    ? Math.round(
-        new Date(data.completedAt).getTime() -
-          new Date(data.startedAt).getTime(),
-      ) / 1000
-    : null;
+      ? Math.round(
+      new Date(data.completedAt).getTime() -
+      new Date(data.startedAt).getTime(),
+  ) / 1000
+      : null;
 
   const subtitle = (
-    <>
-      {data.workflow.name} &bull; Started{" "}
-      {formatDistanceToNow(data.startedAt, { addSuffix: true })}
-      {duration !== null && <> &bull; Took {duration}s </>}
-    </>
+      <>
+        {data.workflow.name} &bull; Started{" "}
+        {formatDistanceToNow(data.startedAt, {addSuffix: true})}
+        {duration !== null && <> &bull; Took {duration}s </>}
+      </>
   );
 
   return (
-    <EntityItem
-      href={`/executions/${data.id}`}
-      title={data.status}
-      subtitle={subtitle}
-      image={
-        <div className={"size-8 flex items-center justify-center"}>
-          {getStatusIcon(data.status)}
-        </div>
-      }
-    />
+      <EntityItem
+          href={`/executions/${data.id}`}
+          title={data.status}
+          subtitle={subtitle}
+          image={
+            <div className={"size-8 flex items-center justify-center"}>
+              {getStatusIcon(data.status)}
+            </div>
+          }
+      />
   );
 };
