685e3999974a2f9f0542808dd2cd2043f2fbbd87
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2025-12-04 09:10:45 +0200

    add google and github providers for authentication

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,8 @@
-import {betterAuth} from "better-auth";
-import {prismaAdapter} from "better-auth/adapters/prisma";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/db";
-import {polar, checkout, portal} from "@polar-sh/better-auth"
-import {polarClient} from "./polar"
+import { polar, checkout, portal } from "@polar-sh/better-auth"
+import { polarClient } from "./polar"
 
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL || process.env.NEXT_PUBLIC_APP_URL || (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000'),
@@ -12,6 +12,15 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
     autoSignIn: true,
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }, google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }
   },
   plugins: [
     polar({
