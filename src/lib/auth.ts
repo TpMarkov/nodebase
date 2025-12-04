6f4c2f76c0b0f0557008ce7ff9a405c49ae2ad71
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2025-12-04 09:24:29 +0200

    change product id for polar payments

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -30,7 +30,7 @@ export const auth = betterAuth({
         checkout({
           products: [
             {
-              productId: "d0723c7d-c9b0-4330-8a21-1002b5c0d801",
+              productId: "aa290361-b01f-4ec5-b3d7-f4130cbeed32",
               slug: "Nodebase Pro"
             }
           ],
