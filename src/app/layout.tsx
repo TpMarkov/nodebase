65e1e030e4ff40eb8d7d7364872a097056e06739
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2025-12-22 16:45:27 +0200

    fix conflict

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
-import type {Metadata} from "next";
-import {Geist, Geist_Mono} from "next/font/google";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import {TRPCReactProvider} from "@/trpc/client";
-import {Toaster} from "sonner";
-import {NuqsAdapter} from "nuqs/adapters/next";
-import {Provider} from "jotai"
+import { TRPCReactProvider } from "@/trpc/client";
+import { Toaster } from "sonner";
+import { NuqsAdapter } from "nuqs/adapters/next";
+import { Provider } from "jotai"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,33 +18,29 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Nodebase",
-<<<<<<< HEAD
   description: "*A production-ready, full-stack workflow automation platform with AI integration and real-time execution monitoring",
-=======
-  description: "A production-ready, full-stack workflow automation platform with AI integration and real-time execution monitoring",
->>>>>>> 7b90b49df99c0bbc4d2466b1ebf85655bdda49cc
 };
 
 export default function RootLayout({
-                                     children,
-                                   }: Readonly<{
+  children,
+}: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
 
-      <html lang="en">
+    <html lang="en">
       <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased max-h-screen`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased max-h-screen`}
       >
-      <TRPCReactProvider>
-        <NuqsAdapter>
-          <Provider>
-            {children}
-          </Provider>
-          <Toaster theme={"dark"}/>
-        </NuqsAdapter>
-      </TRPCReactProvider>
+        <TRPCReactProvider>
+          <NuqsAdapter>
+            <Provider>
+              {children}
+            </Provider>
+            <Toaster theme={"dark"} />
+          </NuqsAdapter>
+        </TRPCReactProvider>
       </body>
-      </html>
+    </html>
   )
 }
